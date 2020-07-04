8c0ae70dce67eb94494789b1c0f57636c037eb2c
Rupa <rupa211984@gmail.com>
2020-07-03 23:57:41 -0700

    Comment out myConnect Test

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import CatPage from './CatPage';
 import Text from './Text';
-import TestMyConnect from './my_connect/TestMyConnect';
+// import TestMyConnect from './my_connect/TestMyConnect';
 
 function App() {
   return (
     <>
     <Text text={'Hi there'} />
     <CatPage />
-    <TestMyConnect extraTest={'Just being extra'} />
+    {/* <TestMyConnect extraTest={'Just being extra'} /> */}
     </>
   );
 }
