e825a5d0ec35960ed6236f24ecfff15b58aba418
Rupa <rupa211984@gmail.com>
2020-07-03 23:57:07 -0700

    Add myConnect

---
+++ src/my_connect/woofyActions.js
@@ -0,0 +1,14 @@
+export const uselessAction = woofy => ({
+  type: 'NO_RESPOND',
+  woofy
+});
+
+export const addWoofy = woofy => ({
+  type: 'ADD_WOOFY',
+  woofy
+});
+
+export const chooseWoofy = woofy => ({
+  type: 'CHOOSE_WOOFY',
+  woofy
+});
